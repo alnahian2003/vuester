da9b3042fe58d2283be376a612f525a291d5be0f
alnahian2003 <a.alnahian2003@gmail.com>
2022-10-21 03:25:07 +0600

    One Vue Component Per File

---
+++ components/appButton.js
@@ -0,0 +1,3 @@
+export default {
+  template: `<button class="bg-[#25925f] hover:bg-green-700 text-white px-4 py-3 text-xl rounded"><slot/></button>`,
+};
---
+++ components/appCard.js
@@ -0,0 +1,18 @@
+export default {
+  template: `
+    <div class="max-w-sm rounded-lg overflow-hidden shadow-lg bg-[#25925f] text-white">
+        <img class="w-full" src="vuejs.png" alt="Sunset in the mountains">
+        <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p class="text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+        </div>
+    </div>
+    `,
+};
