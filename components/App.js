8ce8c50f81af3d09fd7398fd58c20852935d2e76
alnahian2003 <a.alnahian2003@gmail.com>
2022-10-21 14:38:20 +0600

    Component Props

---
+++ components/App.js
@@ -0,0 +1,11 @@
+import AppButton from "/components/AppButton.js";
+
+import AppCard from "/components/AppCard.js";
+
+export default {
+  components: {
+    "app-button": AppButton,
+
+    "app-card": AppCard,
+  },
+};
--- components/appButton.js
+++ components/appButton.js
@@ -1,3 +1,18 @@
 export default {
-  template: `<button class="bg-[#25925f] hover:bg-green-700 text-white px-4 py-3 text-xl rounded"><slot/></button>`,
+  template: `<button :class="{
+    'text-white px-4 py-3 text-xl rounded': true,
+
+    'bg-[#25925f] hover:bg-green-700': type === 'primary',
+    
+    'bg-indigo-600 hover:bg-indigo-700': type === 'secondary',
+
+    'bg-gray-500 hover:bg-gray-300': type === 'muted',
+  }"><slot/></button>`,
+
+  props: {
+    type: {
+      type: String,
+      default: "primary",
+    },
+  },
 };
