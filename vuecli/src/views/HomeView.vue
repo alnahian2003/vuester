a480fd925df57bef36b4b7c6972e00125921eb97
alnahian2003 <a.alnahian2003@gmail.com>
2023-01-22 00:49:23 +0600

    intro to compositions api

--- vuecli/src/views/HomeView.vue
+++ vuecli/src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup>
 import TheWelcome from "../components/TheWelcome.vue";
+import { ref, onMounted } from "vue";
+
+const count = ref(0);
+let incr = ref(1);
+
+function increment(incrementBy = 1) {
+  count.value += incrementBy;
+}
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    {{ count }}
+    <input type="number" v-model="incr" />
+    <button @click="increment(incr)">+</button>
   </main>
 </template>
