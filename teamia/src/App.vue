1b31896df22e45889736dfcc12984aacd4631b17
alnahian2003 <a.alnahian2003@gmail.com>
2023-01-24 17:18:49 +0600

    project initialized, tailwind configured

---
+++ teamia/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header class="bg-slate-800 p-4 md:px-16 md:py-4 text-gray-200">
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
